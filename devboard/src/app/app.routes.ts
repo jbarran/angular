a926dd68ed26fad863cfdc1cf990d9e1889de466
John Barran <jbarran.au@gmail.com>
2025-06-14 14:24:33 +1000

    refactor(paths): unify all imports to use @devboard/ alias prefix

--- devboard/src/app/app.routes.ts
+++ devboard/src/app/app.routes.ts
@@ -4,27 +4,27 @@ export const routes: Routes = [
     {
         path: 'auth',
         loadChildren: () =>
-            import('@angular-devboard/auth').then((m) => m.AUTH_ROUTES),
+            import('@devboard/auth').then((m) => m.AUTH_ROUTES),
     },
     {
         path: 'projects',
         loadChildren: () =>
-            import('@angular-devboard/projects').then((m) => m.PROJECTS_ROUTES),
+            import('@devboard/projects').then((m) => m.PROJECTS_ROUTES),
     },
     {
         path: 'metrics',
         loadChildren: () =>
-            import('@angular-devboard/metrics').then((m) => m.METRICS_ROUTES),
+            import('@devboard/metrics').then((m) => m.METRICS_ROUTES),
     },
     {
         path: 'settings',
         loadChildren: () =>
-            import('@angular-devboard/settings').then((m) => m.SETTINGS_ROUTES),
+            import('@devboard/settings').then((m) => m.SETTINGS_ROUTES),
     },
     {
         path: 'admin',
         loadChildren: () =>
-            import('@angular-devboard/admin').then((m) => m.ADMIN_ROUTES),
+            import('@devboard/admin').then((m) => m.ADMIN_ROUTES),
     },
     {
         path: '',
