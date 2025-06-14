59e363e3bd1093eaf3e5f22b579c9629dce35580
John Barran <jbarran.au@gmail.com>
2025-06-14 15:26:35 +1000

    chore(shared-models): remove redundant models.model.* files

--- devboard/libs/shared/models/src/lib/models.model.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-@Component({
-  selector: 'lib-models',
-  imports: [CommonModule],
-  templateUrl: './models.html',
-})
-export class Models {}
