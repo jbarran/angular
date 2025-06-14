2ffce2a81ed44de33162fff5dcb00185b0aa4161
John Barran <jbarran.au@gmail.com>
2025-06-14 14:20:18 +1000

    refactor(shared): setup ui, utils, and models libraries with clean naming and exports

---
+++ devboard/libs/shared/ui/src/lib/ui.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'devboard-ui',
+  standalone: true,
+  templateUrl: './ui.component.html',
+  styleUrls: ['./ui.component.scss'],
+})
+export class UiComponent {}
