d16e3daa4ca42a29bb24efa488ccd1df50157795
John Barran <jbarran.au@gmail.com>
2025-06-14 20:23:37 +1000

    feat(projects): integrate Angular Material and update UI with Material components
    - Replaced legacy HTML tags with Angular Material components in ProjectsComponent
    - Added Angular Material packages and configured prebuilt theme (azure-blue)
    - Updated project.json to include Material styles and fonts
    - Ensured SSR hydration and zoneless setup continues to function
    - Improved visual consistency and responsiveness using mat-list, mat-button, and Roboto font

--- devboard/libs/projects/src/lib/projects.component.ts
+++ devboard/libs/projects/src/lib/projects.component.ts
@@ -1,10 +1,14 @@
 import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ProjectsService} from "./projects.service";
+import {MatButton, } from '@angular/material/button';
+import {MatList, MatListItem, } from '@angular/material/list';
+import {MatLine} from "@angular/material/core";
 
 @Component({
   selector: 'lib-projects',
-  imports: [CommonModule],
+  imports: [CommonModule, MatListItem, MatButton, MatLine, MatList],
+
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
--- devboard/src/app/app.config.server.ts
+++ devboard/src/app/app.config.server.ts
@@ -7,12 +7,14 @@ import {
 import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { provideClientHydration, withIncrementalHydration } from "@angular/platform-browser";
 
 const serverConfig: ApplicationConfig = {
     providers: [
         provideZonelessChangeDetection(),
         provideBrowserGlobalErrorListeners(),
         provideServerRendering(withRoutes(serverRoutes)),
+        provideClientHydration(withIncrementalHydration()),
     ],
 };
 
